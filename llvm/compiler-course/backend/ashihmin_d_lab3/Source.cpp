08a4e1f82d022d3884415310fd87d6e9710723f3
DaniilAsh <danyapomni7b@mail.ru>
2026-04-29 22:14:33 +0300

    check 5

--- llvm/compiler-course/backend/ashihmin_d_lab3/Source.cpp
+++ llvm/compiler-course/backend/ashihmin_d_lab3/Source.cpp
@@ -3,134 +3,111 @@
 #include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include <vector>
 
 using namespace llvm;
 
 namespace {
+
+struct X86OpInfo {
+  int Change;
+  unsigned AddVariant;
+  unsigned SubVariant;
+  unsigned Width;
+};
 
 class ashihmin_d_lab3 : public MachineFunctionPass {
 public:
   static char ID;
   ashihmin_d_lab3() : MachineFunctionPass(ID) {}
 
-  StringRef getPassName() const override {
-    return "Ashihmin D. Lab3: INC/DEC to ADD/SUB Merger";
-  }
+  StringRef getPassName() const override { return "Ashihmin X86 Arith Folder"; }
 
   bool runOnMachineFunction(MachineFunction &MF) override {
-    const X86InstrInfo *X86TII = MF.getSubtarget<X86Subtarget>().getInstrInfo();
-    bool IsModified = false;
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+    bool Modified = false;
 
     for (auto &MBB : MF) {
       auto MII = MBB.begin();
       while (MII != MBB.end()) {
-        int DeltaAccum = 0;
-        unsigned AddOp, SubOp, RegWidth;
-
-        // Проверяем, является ли инструкция таргетом для замены
-        if (!lookupOpcodeInfo(MII->getOpcode(), DeltaAccum, AddOp, SubOp,
-                              RegWidth)) {
+        X86OpInfo Info;
+        if (!collectInfo(MII->getOpcode(), Info)) {
           ++MII;
           continue;
         }
 
-        Register CurrentReg = MII->getOperand(0).getReg();
+        Register RegToTrack = MII->getOperand(0).getReg();
         DebugLoc DL = MII->getDebugLoc();
-        auto SequenceHead = MII;
 
-        SmallVector<MachineInstr *, 8> SequenceToProcess;
-        int TotalSum = 0;
+        std::vector<MachineInstr *> Sequence;
+        int AccumulatedDelta = 0;
 
-        // Собираем все идущие подряд INC/DEC для этого же регистра
-        while (MII != MBB.end()) {
-          int LocalVal = 0;
-          unsigned TmpA, TmpS, TmpW;
-          if (lookupOpcodeInfo(MII->getOpcode(), LocalVal, TmpA, TmpS, TmpW) &&
-              MII->getOperand(0).getReg() == CurrentReg && TmpW == RegWidth) {
+        auto Scanner = MII;
+        while (Scanner != MBB.end()) {
+          X86OpInfo Current;
+          if (collectInfo(Scanner->getOpcode(), Current) &&
+              Scanner->getOperand(0).getReg() == RegToTrack &&
+              Current.Width == Info.Width) {
 
-            TotalSum += LocalVal;
-            SequenceToProcess.push_back(&*MII);
-            ++MII;
+            AccumulatedDelta += Current.Change;
+            Sequence.push_back(&*Scanner);
+            ++Scanner;
           } else {
             break;
           }
         }
 
-        if (!SequenceToProcess.empty()) {
-          // Если после сложения (например, INC и DEC) получили не ноль
-          if (TotalSum != 0) {
-            unsigned FinalOpcode = (TotalSum > 0) ? AddOp : SubOp;
-            uint64_t Immediate = std::abs(TotalSum);
+        if (!Sequence.empty()) {
+          if (AccumulatedDelta != 0) {
+            unsigned OpcodeToUse =
+                (AccumulatedDelta > 0) ? Info.AddVariant : Info.SubVariant;
+            uint64_t FinalImm = std::abs(AccumulatedDelta);
 
-            BuildMI(MBB, SequenceHead, DL, X86TII->get(FinalOpcode), CurrentReg)
-                .addReg(CurrentReg)
-                .addImm(Immediate)
+            // Вставляем новую инструкцию перед первым элементом пачки
+            BuildMI(MBB, MII, DL, TII->get(OpcodeToUse), RegToTrack)
+                .addReg(RegToTrack)
+                .addImm(FinalImm)
                 .addReg(X86::EFLAGS, RegState::Define | RegState::Implicit);
           }
 
-          // Удаляем старые инструкции в любом случае (даже если TotalSum == 0)
-          for (auto *Inst : SequenceToProcess) {
-            Inst->eraseFromParent();
+          // Удаляем старые инструкции
+          for (auto *MI : Sequence) {
+            MI->eraseFromParent();
           }
-          IsModified = true;
+          Modified = true;
+          MII = Scanner;
         }
       }
     }
-    return IsModified;
+    return Modified;
   }
 
 private:
-  // Маппинг инструкций X86
-  bool lookupOpcodeInfo(unsigned Op, int &Val, unsigned &A, unsigned &S,
-                        unsigned &W) const {
-    switch (Op) {
+  bool collectInfo(unsigned Opc, X86OpInfo &P) const {
+    switch (Opc) {
     case X86::INC8r:
-      Val = 1;
-      A = X86::ADD8ri;
-      S = X86::SUB8ri;
-      W = 8;
+      P = {1, X86::ADD8ri, X86::SUB8ri, 8};
       return true;
     case X86::DEC8r:
-      Val = -1;
-      A = X86::ADD8ri;
-      S = X86::SUB8ri;
-      W = 8;
+      P = {-1, X86::ADD8ri, X86::SUB8ri, 8};
       return true;
     case X86::INC16r:
-      Val = 1;
-      A = X86::ADD16ri;
-      S = X86::SUB16ri;
-      W = 16;
+      P = {1, X86::ADD16ri, X86::SUB16ri, 16};
       return true;
     case X86::DEC16r:
-      Val = -1;
-      A = X86::ADD16ri;
-      S = X86::SUB16ri;
-      W = 16;
+      P = {-1, X86::ADD16ri, X86::SUB16ri, 16};
       return true;
     case X86::INC32r:
-      Val = 1;
-      A = X86::ADD32ri;
-      S = X86::SUB32ri;
-      W = 32;
+      P = {1, X86::ADD32ri, X86::SUB32ri, 32};
       return true;
     case X86::DEC32r:
-      Val = -1;
-      A = X86::ADD32ri;
-      S = X86::SUB32ri;
-      W = 32;
+      P = {-1, X86::ADD32ri, X86::SUB32ri, 32};
       return true;
     case X86::INC64r:
-      Val = 1;
-      A = X86::ADD64ri32;
-      S = X86::SUB64ri32;
-      W = 64;
+      P = {1, X86::ADD64ri32, X86::SUB64ri32, 64};
       return true;
     case X86::DEC64r:
-      Val = -1;
-      A = X86::ADD64ri32;
-      S = X86::SUB64ri32;
-      W = 64;
+      P = {-1, X86::ADD64ri32, X86::SUB64ri32, 64};
       return true;
     default:
       return false;
